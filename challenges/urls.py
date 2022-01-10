f67bb4f64b1da86d2a34acca44d41142cc24eefa
Jae <82623058+Jaegray@users.noreply.github.com>
2022-01-10 13:50:10 -0500

    Update urls.py

--- challenges/urls.py
+++ challenges/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),  # will trigger for /challenges/
     path("<int:month>", views.monthly_challenge_by_number),  # integer values by user will trigger this view
     path("<str:month>", views.monthly_challenge, name="month-challenge")
-    # this is a placeholder and the placeholder word of your choice
+    # this is a placeholder and the placeholder can be a word of your choice
 
 ]  # this is a url config. No longer need to list every month because of the placeholder
